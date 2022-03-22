8ed1c290a0eae7182ef0fd794222e84678c2a72d
thuynguyen2101 <thuy21011991@gmail.com>
2022-03-22 23:26:57 +0700

    064

--- src/lesson_06/Lab_064.java
+++ src/lesson_06/Lab_064.java
@@ -3,8 +3,20 @@ package lesson_06;
 public class Lab_064 {
     public static void main(String[] args) {
 //        LAB 6.4
-//        String url = "https://google.com";
+     String url = "https://google.com";
 //        Check http OR https; domain name, .com OR .net
+
+        if(url.indexOf("https")==0){
+            System.out.println("Here is https");
+        }else {
+            System.out.println("Here is http");
+        }
+
+        if(url.indexOf(".com")==0){
+            System.out.println("Here is .com");
+        }else {
+            System.out.println("Here is .net");
+        }
 
 
     }
---
+++ src/lesson_06/StringHandler.java
@@ -0,0 +1,12 @@
+package lesson_06;
+
+public class StringHandler {
+    public static void main(String[] args) {
+
+        //Access modifier
+        public static String extractNumber(String orginalString){
+
+        }
+
+    }
+}
