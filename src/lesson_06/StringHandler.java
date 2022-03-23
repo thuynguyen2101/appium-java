ed7b7be63d406490daf9a3d844c066bf1de0fbce
Bẹnamin <nguyenquocbao.1999443@gmail.com>
2022-03-23 17:00:20 +0700

    'comment'

--- src/lesson_06/StringHandler.java
+++ src/lesson_06/StringHandler.java
@@ -1,12 +1,32 @@
 package lesson_06;
 
+import java.security.SecureRandom;
+
 public class StringHandler {
-    public static void main(String[] args) {
-
-        //Access modifier
-        public static String extractNumber(String orginalString){
-
-        }
-
+    public static void main(String[] args)  {
+//        System.out.println(extractNumber("203fiosfuosfuoiof"));
+//        System.out.println(extractNumber(""));
+//        System.out.println(extractNumber(null));
+        saySth();
     }
+  public static String extractNumber(String originalStr) {
+        if(originalStr==null ){
+            throw new IllegalArgumentException("Proved str is null");
+        }
+        if(originalStr.length()==0){
+            return "";
+        }
+      return originalStr.replaceAll("[^0-9]","");
+    }
+
+
+    public static int generateRandomNumber(){
+      return new SecureRandom().nextInt(Integer.MAX_VALUE);
+     }
+
+     public static void saySth(){
+         System.out.println("I found the number %d for you: "+generateRandomNumber());
+     }
+
+     //parameters and argument
 }
